b856ee011dffd974c622d859cd4f090bc98d2f77
toshiiu <toshi0110lychee@gmail.com>
2020-11-22 16:22:00 +0900

    configの情報書き換え

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `tosiiu blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `tosiiu`,
+      summary: `UI/UXデザイナー。アプリ開発したりもする人。ガジェットとインターネットが好き。`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `UI/UXデザイナーの好きなものをおすすめしたりするブログ。`,
+    siteUrl: `https://blog.tosiiu.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `tosiiu`,
     },
   },
   plugins: [
